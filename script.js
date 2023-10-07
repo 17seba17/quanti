1aef9cbd1c0619e6e39262371eaffb0e44e31cb9
Nicola <nicola.sebastianutto@gmail.com>
2023-10-07 20:59:45 +0200

    add first js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const searchForm = document.getElementById("search-form");
+    const searchInput = document.getElementById("search-input");
+    const searchResults = document.getElementById("search-results");
+    const resultsSection = document.querySelector(".results-section");
+
+    searchForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        // Esegui una richiesta al server per ottenere i dati rari
+        // e aggiungi i risultati a searchResults
+
+        // Mostra la sezione dei risultati
+        resultsSection.style.display = "block";
+    });
+});
