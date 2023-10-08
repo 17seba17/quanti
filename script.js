a3bba37d3e094c3fd89ea4bf1da3065e5a22db86
Nicola <nicola.sebastianutto@gmail.com>
2023-10-08 12:48:45 +0200

    merge

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     const searchForm = document.getElementById("search-form");
-    const searchInput = document.getElementById("search-input");
     const searchResults = document.getElementById("search-results");
-    const resultsSection = document.querySelector(".results-section");
 
-    searchForm.addEventListener("submit", function (e) {
-        e.preventDefault();
+    searchForm.addEventListener("submit", function(event) {
+        event.preventDefault(); // Impedisci il comportamento predefinito del modulo di invio
 
-        // Esegui una richiesta al server per ottenere i dati rari
-        // e aggiungi i risultati a searchResults
+        // Ottieni la data e l'ora correnti
+        const today = new Date();
+        const formattedDate = `${today.toLocaleDateString()} ${today.toLocaleTimeString()}`;
 
-        // Mostra la sezione dei risultati
-        resultsSection.style.display = "block";
+        // Mostra la stringa dei risultati iniziale
+        searchResults.innerHTML = `Gli articoli del giorno sono: ${formattedDate}<br><br>`;
+
+        // Ottieni l'elenco dei file nella repository utilizzando la GitHub API
+        const repositoryName = "17seba17/quanti"; // Sostituisci con il tuo nome utente GitHub e il nome della repository
+        fetch(`https://api.github.com/repos/${repositoryName}/contents`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.length === 0) {
+                    searchResults.innerHTML += "Nessun file trovato nella repository.";
+                } else {
+                    searchResults.innerHTML += "Elenco dei file nella repository:<br>";
+                    const ul = document.createElement("ul");
+                    data.forEach(item => {
+                        if (item.type === "file") {
+                            const li = document.createElement("li");
+                            const link = document.createElement("a");
+                            link.href = item.html_url;
+                            link.textContent = item.name;
+                            li.appendChild(link);
+                            ul.appendChild(li);
+                        }
+                    });
+                    searchResults.appendChild(ul);
+                }
+            })
+            .catch(error => {
+                console.error("Errore nell'ottenere l'elenco dei file:", error);
+                searchResults.innerHTML += "Errore nell'ottenere l'elenco dei file.";
+            });
     });
 });
