649a4f608fd4e92b7fb0efda07d6d7614674087b
Nicola <nicola.sebastianutto@gmail.com>
2023-10-10 09:26:42 +0200

    easier translation

--- language.js
+++ language.js
@@ -3,8 +3,7 @@
 // Definisci una variabile per la lingua corrente
 let currentLanguage = 'italian';
 
-// Funzione per caricare le stringhe di testo dalla lingua selezionata
-
+// Funzione per cambiare la lingua
 function changeLanguage() {
     // Cambia la lingua corrente
     currentLanguage = currentLanguage === 'italian' ? 'english' : 'italian';
@@ -14,19 +13,20 @@ function changeLanguage() {
 }
 
 function loadLanguageFile() {
-    const languageFile = currentLanguage === 'italian' ? 'ita.json' : 'en.json';
-
-    fetch(languageFile)
+    fetch('file.json') // Sostituisci 'file.json' con il percorso del tuo nuovo file JSON
         .then(response => response.json())
         .then(data => {
             // Imposta il testo degli elementi HTML utilizzando i dati caricati
-            document.getElementById('header-title').textContent =data.headerTitle;
-            document.getElementById('header-subtitle').textContent = data.headerSubtitle;
-            document.getElementById('search-input').placeholder = data.searchPlaceholder;
-            document.getElementById('search-form').querySelector('button').textContent = data.searchButton;
-            document.getElementById('footer-text').innerHTML = data.footerText;
+            document.getElementById('header-title').textContent = data.headerTitle[currentLanguage];
+            document.getElementById('header-subtitle').textContent = data.headerSubtitle[currentLanguage];
+            document.getElementById('search-input').placeholder = data.searchPlaceholder[currentLanguage];
+            document.getElementById('search-form').querySelector('button').textContent = data.searchButton[currentLanguage];
+            document.getElementById('footer-text').innerHTML = data.footerText[currentLanguage];
         })
         .catch(error => {
             console.error('Errore nel caricamento del file di lingua:', error);
         });
 }
+
+// Chiamare questa funzione all'avvio per impostare la lingua iniziale
+loadLanguageFile();
